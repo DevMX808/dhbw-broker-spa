61906cd984a0de33b389656ad9139079c26320f1
DevMX808 <mx-dev@outlook.com>
2025-10-05 20:33:40 +0200

    server.js splat fix

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/dhbw-broker-web/browser')));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'dist/dhbw-broker-web/browser', 'index.html'))
-);
+app.get('/{*splat}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/dhbw-broker-web/browser', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server running on port ${process.env.PORT || 8080}`);
