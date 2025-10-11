08edd964d0b149555eabb37d57ffd212d0589a5e
DevMX808 <mx-dev@outlook.com>
2025-10-11 13:08:35 +0200

    port switch for local dev

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/dhbw-broker-web/browser', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Server running on port ${process.env.PORT || 8080}`);
+app.listen(process.env.PORT || 4200, () => {
+  console.log(`Server running on port ${process.env.PORT || 4200}`);
 });
