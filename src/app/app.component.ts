04132458210b768b165e7a87b331dd388fd3b663
DevMX808 <mx-dev@outlook.com>
2025-10-11 13:22:09 +0200

    core service api test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dhbw-broker-web';
+  health = '';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getHealth().subscribe({
+      next: (response) => this.health = response,
+      error: (err) => console.error('Error:', err)
+    });
+  }
 }
